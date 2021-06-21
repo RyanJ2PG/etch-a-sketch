f469db2c05f73b70cb4102c9fd1396df2e1d6ada
Ryan Herbert <RyanJ2PG@gmail.com>
2021-06-21 15:17:07 -0400

    created a nested for loop for creating 16x16 squared and created a grid to put them into

--- main.js
+++ main.js
@@ -1,15 +1,21 @@
 let gridContainer = document.querySelector('#gridContainer');
-gridContainer.style.cssText = 'border: 1px solid black'
+gridContainer.style.cssText = 'border: 0px solid black'
 
+//function for making 1 square
 function squareMaker(){
     let divGrid = document.createElement('div');
     divGrid.classList.add('square');
-    divGrid.style.cssText = 'background-color: red; width: 50px; height: 50px; border: 1px solid black;'
+    divGrid.style.cssText = 'background-color: white; width: 50px; height: 50px; border: 1px solid black;'
     gridContainer.appendChild(divGrid)
 }
 
+//loop creating 256 squares
 for (i = 0; i < 16; i++){
+
+    for (j = 0; j < 16; j++){
+    
     squareMaker();
+    }
 }
 
 
