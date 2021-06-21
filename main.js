3c9361546a443f277bc08f4b131cd0db85556114
Ryan Herbert <RyanJ2PG@gmail.com>
2021-06-21 19:14:46 -0400

    created a function for making buttons and added a function to add a draw on click

--- main.js
+++ main.js
@@ -1,4 +1,8 @@
 let gridContainer = document.querySelector('#gridContainer');
+let isClicked = false; //**CHEATING**
+let color = "black";
+
+
 
 //function for making 1 square
 function SquareMaker(){
@@ -7,6 +11,8 @@ function SquareMaker(){
     divGrid.style.cssText = 'background-color: white; width: 50px; height: 50px; border: 1px solid black;'
     gridContainer.appendChild(divGrid)
     divGrid.addEventListener("mouseover", ChangeColor);
+    divGrid.addEventListener("click", CheckIfClicked); //**CHEATING**
+
 }
 
 //loop creating 256 squares
@@ -18,12 +24,52 @@ for (i = 0; i < 16; i++){
     }
 }
 
-
-function ChangeColor(){
-    this.style.backgroundColor = "black";
+//**CHEATING**
+function CheckIfClicked(e){
+    if (e.buttons == 0){
+        isClicked = !isClicked;
+        this.style.backgroundColor = color;
+    }
 }
 
+//changes the color of the divs
+function ChangeColor(){
+    if (isClicked){
+        this.style.backgroundColor = color;
+    }
+}
+
+function SetNewColor(newColor){
+    color = newColor;
+}
+
+function CreateBtn(name, color, clear){
+    let tmpBtn = document.createElement('button');
+    let tmpBtnText = document.createTextNode(name);
+    tmpBtn.appendChild(tmpBtnText);
+    document.body.appendChild(tmpBtn);
+    tmpBtn.type = 'button';
+    tmpBtn.classList.add(name.toLowerCase());
+    tmpBtn.addEventListener('click', function(){
+        SetNewColor(color);
+        isClicked = false;
+        if(clear){
+            ClearGrid();
+        }
+    });
+}
+CreateBtn("Reset", "black", true);
+CreateBtn("Red", "red", false,);
+CreateBtn("Black", "black", false);
+CreateBtn("Eraser", "white", false);
+
+
+
+
+//resets the divs back to white
 function ClearGrid(){
-    document.querySelectorAll('square');
-    
+    let divList = gridContainer.getElementsByClassName('square');
+    for (i = 0; i < divList.length; i++){
+        divList[i].style.backgroundColor = "white";
+    }  
 }
