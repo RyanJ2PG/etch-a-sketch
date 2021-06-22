73cdd660f48cb4b979fb4a5c92e6672d39c1707c
Ryan Herbert <RyanJ2PG@gmail.com>
2021-06-22 00:44:56 -0400

    created a slider with range values, added a resiazeble grid function dependent on range values, and created buttons that change color when clicked, this is the final project

--- main.js
+++ main.js
@@ -1,24 +1,53 @@
 let gridContainer = document.querySelector('#gridContainer');
+let sliderContainer = document.querySelector('.sliderContainer');
 let isClicked = false; //**CHEATING**
 let color = "black";
+let currentlyActiveBtn;
 
+let slider = document.createElement('input')
+slider.setAttribute("type", "range");
+slider.classList.add("rangeInput");
+slider.min = 1;
+slider.max = 40;
+slider.defaultValue = 16;
+slider.addEventListener('input', function(){
+    textValue.textContent = "Grid Size: " + slider.value;
+    GridSize();  
+});
+
+gridContainer.style.cssText = `display: grid; grid-template-columns: repeat(${slider.value}, 1fr); grid-template-rows: repeat(${slider.value}, 1fr); justify-content: center; width: 900px;  height: 900px;`
 
 
 //function for making 1 square
 function SquareMaker(){
     let divGrid = document.createElement('div');
     divGrid.classList.add('square');
-    divGrid.style.cssText = 'background-color: white; width: 50px; height: 50px; border: 1px solid black;'
+    divGrid.style.cssText = 'background-color: white; width: 100%; height: 100%; border: 0.1px solid black;'
     gridContainer.appendChild(divGrid)
     divGrid.addEventListener("mouseover", ChangeColor);
     divGrid.addEventListener("click", CheckIfClicked); //**CHEATING**
 
 }
 
-//loop creating 256 squares
-for (i = 0; i < 16; i++){
+function GridSize(){
+    let deleteSquares = document.getElementsByClassName('square');
+    for (i = deleteSquares.length-1; i >= 0; i--){
+        gridContainer.removeChild(deleteSquares[i]);
+    };
+    gridContainer.style.cssText = `display: grid; grid-template-columns: repeat(${slider.value}, 1fr); grid-template-rows: repeat(${slider.value}, 1fr); justify-content: center; width: 900px;  height: 900px;`
+    for (i = 0; i < slider.value; i++){
 
-    for (j = 0; j < 16; j++){
+        for (j = 0; j < slider.value; j++){
+        
+        SquareMaker();
+        }
+    } 
+}
+
+// loop talking about 256 squares
+for (i = 0; i < slider.value; i++){
+
+    for (j = 0; j < slider.value; j++){
     
     SquareMaker();
     }
@@ -48,14 +77,20 @@ function CreateBtn(name, color, clear){
     let tmpBtnText = document.createTextNode(name);
     tmpBtn.appendChild(tmpBtnText);
     document.body.appendChild(tmpBtn);
+    tmpBtn.style.backgroundColor = "white";
     tmpBtn.type = 'button';
     tmpBtn.classList.add(name.toLowerCase());
     tmpBtn.addEventListener('click', function(){
+        if(currentlyActiveBtn){
+            currentlyActiveBtn.style.backgroundColor = "white";
+        }
         SetNewColor(color);
         isClicked = false;
+        tmpBtn.style.backgroundColor = "#add8e6";
         if(clear){
             ClearGrid();
         }
+        currentlyActiveBtn = tmpBtn;
     });
 }
 CreateBtn("Reset", "black", true);
@@ -63,8 +98,11 @@ CreateBtn("Red", "red", false,);
 CreateBtn("Black", "black", false);
 CreateBtn("Eraser", "white", false);
 
+document.body.append(slider);
 
-
+let textValue = document.createElement('p');
+document.body.appendChild(textValue);
+textValue.textContent = "Grid Size: " + slider.value;  
 
 //resets the divs back to white
 function ClearGrid(){
